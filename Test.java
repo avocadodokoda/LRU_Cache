a91a1dba56a35d471d91ffb7c6f9f6f5077703b4
avocado <ssp.10.01e@gmail.com>
2023-06-23 12:37:08 +0900

    mapを使ってみる。

--- Test.java
+++ Test.java
@@ -2,7 +2,16 @@ import lruPackage.LRU_Cache;
 
 public class Test {
     public static void main(String[] args){
+        Object value;
         LRU_Cache lru = new LRU_Cache();
-        lru.put();
+        lru.put("a", "apple");
+        lru.put("b", "banana");
+        lru.put("c", "cherry");
+        String[] allay = {"a", "b", "c"};
+        for (int i = 0; i < allay.length; i++){
+            value = lru.get(allay[i]);
+            System.out.println("key:" + i);
+            System.out.println("value:" + value);
+        }
     }
 }
--- lruPackage/LRU_Cache.java
+++ lruPackage/LRU_Cache.java
@@ -1,7 +1,18 @@
 package lruPackage;
 
-public class  LRU_Cache{
-    public void put(){
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRU_Cache{
+    public int size;
+    public Map<Object, Object> map1 = new HashMap<>();
+    public void put(Object key, Object value){
+        map1.put(key, value);
         System.out.println("put!");
+    }
+
+    public Object get(Object key){
+        Object value = map1.get(key);
+        return value;
     }
 }
