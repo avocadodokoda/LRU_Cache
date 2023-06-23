de3ab9921413277224e9ffc75baa397fe2ec666f
avocado <ssp.10.01e@gmail.com>
2023-06-23 12:04:05 +0900

    パッケージを使ってみる

--- Test.java
+++ Test.java
@@ -1,26 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
-
-import LRU_Cache.lru;
+import lruPackage.LRU_Cache;
 
 public class Test {
-    Package LRU;
     public static void main(String[] args){
-        LRU_Cache lru_Cache = new LRU_Cache();
-        lru
-        Object result;
-        LRU_Cache.lru.put("a", "apple");
-        LRU_Cache.lru.put("b", "banana");
-        result = LRU_Cache.lru.get("a");
-        System.out.println(result);
-        LRU_Cache.lru.put("c", "cherry");
-        result = LRU_Cache.lru.get("c");
-        System.out.println(result);
-
-        Map<String, String> testMap = new HashMap<>();
-        testMap.put("a", "apple");
-        String aa = testMap.get("a");
-        System.out.println(aa);
-        
+        LRU_Cache lru = new LRU_Cache();
+        lru.put();
     }
 }
---
+++ lruPackage/LRU_Cache.java
@@ -0,0 +1,7 @@
+package lruPackage;
+
+public class  LRU_Cache{
+    public void put(){
+        System.out.println("put!");
+    }
+}
